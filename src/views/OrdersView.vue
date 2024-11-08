9f1b012e7ce034452a415a30d91244ddb3ddeb9e
JustinMassart <justin.massart@student.hepl.be>
2024-11-08 10:34:43 +0100

    Added orders view page content

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,9 +1,52 @@
 <script lang="ts" setup>
-//
+const orders = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 </script>
 
 <template>
-  <main>
-    <h1>Orders View</h1>
+  <main class="orders-list-container">
+    <section class="waiting_orders orders-list card">
+      <div v-for="(order, index) in orders" :key="index" class="order">
+        <div class="order__items">
+          <p class="order__items__name">Margarita</p>
+          <p class="order__items__quantity">1x</p>
+        </div>
+        <p>John Doe</p>
+        <p>12:03:05</p>
+        <p>Waiting</p>
+      </div>
+    </section>
+    <section class="started_orders orders-list card">
+      <div v-for="(order, index) in orders" :key="index" class="order">
+        <div class="order__items">
+          <p class="order__items__name">Margarita</p>
+          <p class="order__items__quantity">1x</p>
+        </div>
+        <p>John Doe</p>
+        <p>12:03:05</p>
+        <p>started</p>
+      </div>
+    </section>
+    <section class="ready_orders orders-list card">
+      <div v-for="(order, index) in orders" :key="index" class="order">
+        <div class="order__items">
+          <p class="order__items__name">Margarita</p>
+          <p class="order__items__quantity">1x</p>
+        </div>
+        <p>John Doe</p>
+        <p>12:03:05</p>
+        <p>ready</p>
+      </div>
+    </section>
+    <section class="delivered_orders orders-list card">
+      <div v-for="(order, index) in orders" :key="index" class="order">
+        <div class="order__items">
+          <p class="order__items__name">Margarita</p>
+          <p class="order__items__quantity">1x</p>
+        </div>
+        <p>John Doe</p>
+        <p>12:03:05</p>
+        <p>Delivered at 12:05:55</p>
+      </div>
+    </section>
   </main>
 </template>
