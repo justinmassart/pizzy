d4507f1979591db341bf99d08cb2aef7ee89ecde
JustinMassart <justin.massart@student.hepl.be>
2024-11-08 17:20:37 +0100

    Added OrderModal

---
+++ src/components/modals/OrderModal.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import TheModal from '@/components/modals/TheModal.vue'
+import FormField from '@/components/FormField.vue'
+</script>
+
+<template>
+  <TheModal modal-title="Create an order">
+    <div class="form">
+      <FormField id="customer" input-type="text" label="Customer" placeholder="John Doe" />
+      <button class="btn-main">Create order</button>
+    </div>
+  </TheModal>
+</template>
