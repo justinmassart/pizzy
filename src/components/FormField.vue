ca4aa8bcea442bb3b82d2f72d1756b8c70c1d9a8
JustinMassart <justin.massart@student.hepl.be>
2024-11-08 17:20:47 +0100

    Added FormField component

---
+++ src/components/FormField.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+  label: string
+  id: string
+  placeholder: string
+  inputType: string
+}>()
+</script>
+
+<template>
+  <div class="form__field">
+    <label :for="id" class="form__field__label">{{ label }}</label>
+    <input :id :placeholder :type="inputType" class="form__field__input" />
+  </div>
+</template>
