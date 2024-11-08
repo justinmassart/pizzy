31ad9f6f9a15fd78a474bdf9a64da37d19111e51
JustinMassart <justin.massart@student.hepl.be>
2024-11-08 17:20:24 +0100

    Added main modal frame

---
+++ src/components/modals/TheModal.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+
+defineProps<{
+  modalTitle: string
+  buttonText?: string
+}>()
+</script>
+
+<template>
+  <button class="btn-main btn-add-new" @click="open = true">{{ buttonText ?? modalTitle }}</button>
+  <div v-if="open" class="modal">
+    <div class="modal__body">
+      <div class="modal__header">
+        <p class="modal__title">{{ modalTitle }}</p>
+        <div class="modal__close" @click="open = false"></div>
+      </div>
+      <div class="modal__main">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
